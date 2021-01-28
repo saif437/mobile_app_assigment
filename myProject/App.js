0c27cad79fe1faee2602e1e26200f3ea096d0ab4
saif437 <saifali_16@outlook.com>
2021-01-28 16:00:16 +0000

    Tab navigation

--- myProject/App.js
+++ myProject/App.js
@@ -4,13 +4,15 @@ import React, { Component } from 'react'
 import { Text, TextInput, View, TouchableOpacity, StyleSheet, Alert } from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomNavigator } from '@react-navigation/Bottom-tabs'
+
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 import HomeScreen from './components/HomeScreen'
 import About from './components/About'
 import Contact from './components/Contact'
 
-const Stack = createStackNavigator()
+const Tab = createTabNavigator()
 
 class App extends Component {
   constructor (props) {
@@ -20,11 +22,11 @@ class App extends Component {
   render () {
     return (
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='Home' component={HomeScreen} />
-          <Stack.Screen name='About' component={About} options={{ title: 'Our story' }} />
-          <Stack.Screen name='Contact' component={Contact} />
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name='Home' component={HomeScreen} />
+          <Tab.Screen name='About' component={About} options={{ title: 'Our story' }} />
+          <Tab.Screen name='Contact' component={Contact} />
+        </Tab.Navigator>
       </NavigationContainer>
     )
   }
